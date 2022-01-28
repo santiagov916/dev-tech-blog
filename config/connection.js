903d5ff84aa14c8f257a733c928983c2714d2ae7
Santiago Venegas <santiagovenegas@Santiagos-MacBook-Pro.local>
2022-01-28 14:40:44 -0800

    debug commit

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ const sequelize = process.env.JAWSDB_URL
     : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: 'localhost',
         dialect: 'mysql',
-        port: 3004
+        port: 5505
     });
 
     module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 
-const PORT = process.env.PORT || 3004;
+const PORT = process.env.PORT || 8801;
 
 const sequelize = require('./config/connection');
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
 
