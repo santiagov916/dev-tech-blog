0f347cdd3123a2e5349c414e8d07441f24ab12b2
Santiago Venegas <santiagovenegas@Santiagos-MacBook-Pro.local>
2022-01-28 16:13:45 -0800

    model index updated with associations

--- models/index.js
+++ models/index.js
@@ -2,4 +2,37 @@ const User = require('./User');
 const Comment = require('./Comment');
 const Post = require('./Post');
 
+// User associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+// Post associations
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Post.hasMany(Comment.hasMany, {
+    foreignKey: post_id
+});
+
+// Comment associations
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+}),
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
+});
+
+
+
 module.exports = { User, Post, Comment };
