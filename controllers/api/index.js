bc52d92f3e19bd638b5f225d930d0dc5964b30f2
Santiago Venegas <santiagovenegas@Santiagos-MacBook-Pro.local>
2022-02-01 14:54:10 -0800

    final touches for seeding database

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 
+
 const userRoutes = require('./user-routes');
 router.use('/user', userRoutes);
 
 const postRoutes = require('./post-routes');
 router.use('/post', postRoutes);
 
-const commentRoute = require('./comment-routes');
-router.use('/comment', commentRoute);
+const commentRoutes = require('./comment-routes');
+router.use('/comment', commentRoutes);
+
 
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 
- console.log('home routes open');
+ console.log('home-routes open');
 
  module.exports = router;
