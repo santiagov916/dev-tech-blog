e4f45a7e72438d05434e71020c997464325cf111
Santiago Venegas <santiagovenegas@Santiagos-MacBook-Pro.local>
2022-02-02 20:52:55 -0800

    post route ready '

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -4,7 +4,7 @@ const { Post, User, Comment } = require('../../models');
 
 router.post('/', withAuth, (req, res) => {
     const body = req.body;
-    Post.create({ ...body, userId: req.session.userId})
+    Post.create({ ...body, user_id: req.session.user_id })
     .then(newPost => {
         res.json(newPost);
     })
@@ -14,4 +14,52 @@ router.post('/', withAuth, (req, res) => {
     });
 });
 
+router.put('/:id', (req, res) => {
+    Post.update(req.body, {
+        where: {
+            id:req.params.id
+        }
+    })
+    .then(affectedRows => {
+        if (!affectedRows > 0) {
+            res.status(200).end();
+        } else {
+            res.status(404).end();
+        }
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/', (req, res) => {
+    Post.findAll({})
+    .then(allPost => {
+        res.json(allPost);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(affectedRows => {
+        if (affectedRows > 0) {
+            res.status(200).end();
+        } else {
+            res.status(404).end();
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 module.exports = router;
--- models/Post.js
+++ models/Post.js
@@ -3,34 +3,14 @@ const sequelize = require('../config/connection');
 
 class Post extends Model {}
 
-Post.init({
-    id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
+Post.init(
+    {
+      title: DataTypes.STRING,
+      body: DataTypes.STRING
     },
-    title: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    body: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    user_id: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: 'user',
-            key: 'id'
-        }
+    {
+      sequelize
     }
-},
-{
-    sequelize,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'post'
-});
+  );
 
 module.exports = Post;
