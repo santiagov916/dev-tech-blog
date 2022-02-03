10924b54fedf97724c6a91cc7d92fa2ae6258e55
Santiago Venegas <santiagovenegas@Santiagos-MacBook-Pro.local>
2022-02-02 21:03:33 -0800

    comment route ready

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,5 +1,17 @@
 const router = require('express').Router();
+const { Comment } = require('../../models');
 
 console.log('comment-routes open');
+
+router.post('/', (req, res) => {
+    Comment.create({ ...req.body, userId: req.session.userId})
+    .then(newComment => {
+        res.json(newComment);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
